3c30c3cb36be873893055d413a723ed760e07deb
harun5897 <harungb025@gmail.com>
2020-10-20 20:49:14 +0700

    bug

--- user_list.php
+++ user_list.php
@@ -40,7 +40,7 @@ if(isset($_GET['hal'])) {
 }
 if(isset($_POST['b_save_user'])) {
 
-    user_save($koneksi, $_POST['nik'], $_POST['name'], $_POST['email'], $_POST['password'],);
+    user_save($koneksi, $_POST['nik'], $_POST['name'], $_POST['email'], $_POST['password']);
 }
 ?>
 <body class="body">
